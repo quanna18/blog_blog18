47d71767b60b0d29bed432085d2b4e62b4e8b6b8
shill@chicagotechacademy.org <shill@chicagotechacademy.org>
2013-12-19 15:10:02 -0600

    intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBlog::Application.config.secret_token = '95bb5ea678e3c59eecf7fa5f2c834650199c267abe6aa12660e2b8ac6a3a9f3f35ad85227370c3b93972a81c8633dd2a061fd2b9d9985ef61d38d2c60c55f2d3'
